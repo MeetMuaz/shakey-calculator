46f2e43e9df45d2ef2a40197ef7e6c6843350794
MeetMuaz <muazbalogun97@gmail.com>
2024-06-27 23:59:58 +0100

    sreen layout created

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,12 @@
-import { Dimensions, StyleSheet, View } from "react-native";
-
+import { Dimensions, StyleSheet, View, StatusBar } from "react-native";
 
 // distructure the Dimensions component to get height and width of the mobile screen
 const { width, height } = Dimensions.get('screen');
+
+
+// get the height of the status bar
+const statusBarHeight = StatusBar.currentHeight;
+
 
 export default function Index() {
   return (
@@ -31,9 +35,14 @@ const style = StyleSheet.create({
     flex: 1,
   },
   displayScreen: {
+    flex: 2,
     width: width,
+    backgroundColor: '#FFC2B3',
+    paddingTop: statusBarHeight, // Ensures content does not overlap with the status bar, accounting for devices with notches
   },
   buttonsContainer: {
+    flex: 3,
     width: width,
+    backgroundColor: '#FF8E71',
   }
 })
